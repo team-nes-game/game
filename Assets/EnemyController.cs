4d1829d6b7fe4580527a32fa3147824e9d0b5474
Sam <swsvindland@gmail.com>
2019-05-01 21:54:46 -0700

    Auto stash before merge of "feature/demo" and "origin/feature/demo"

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    public float maxDist;
+    public float minDist;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.position * speed * Time.deltaTime;
+ 
+    }
+}
