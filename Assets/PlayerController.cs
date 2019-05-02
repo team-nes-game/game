42330396a8b4e4ce389aecbf9fd22a457c358a4b
Ethan Quick <efquick97@gmail.com>
2019-05-01 22:46:39 -0700

    Auto stash before merge of "feature/demo" and "origin/feature/demo"

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public int MAX_HEALTH;
+    public GameObject self;
+    private int cur_health;
+    private bool alive;
 
     // Start is called before the first frame update
     void Start()
     {
+    	cur_health = MAX_HEALTH;
+    	alive = true;
     }
 
     private void FixedUpdate()
     {
+    	if(cur_health <= 0){
+    		print("Player killed");
+    		alive = false;
+    	}
+
         Vector2 pos = transform.position;
 
         if (Input.GetKey("w"))
