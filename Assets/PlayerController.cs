1cdefc8fd30ff72eba9dba270c378f258f465d44
Ethan Quick <efquick97@gmail.com>
2019-05-02 22:23:05 -0700

    Added simple animations
    They need some work

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,15 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public int MAX_HEALTH;
-    public GameObject self;
+    private Rigidbody2D self;
     private int cur_health;
     private bool alive;
+    private Animator animator;
+    private Vector3 movement;
 
     // Start is called before the first frame update
     void Start()
     {
     	cur_health = MAX_HEALTH;
     	alive = true;
+    	animator = GetComponent<Animator>();
+    	self = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
@@ -26,24 +30,42 @@ public class PlayerController : MonoBehaviour
 
         Vector2 pos = transform.position;
 
+        movement = Vector3.zero;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if(movement != Vector3.zero){
+        	self.MovePosition(transform.position + movement * speed * Time.deltaTime);
+        	animator.SetFloat("MoveX", movement.x);
+        	animator.SetFloat("MoveY", movement.y);
+        	animator.SetBool("Walking", true);
+        }
+        else{
+        	animator.SetBool("Walking", false);
+        }
+        /*
         if (Input.GetKey("w"))
         {
             pos.y += speed * Time.deltaTime;
+            animator.SetFloat("MoveY", 1);
         }
         if (Input.GetKey("s"))
         {
             pos.y -= speed * Time.deltaTime;
+            animator.SetFloat("MoveY", -1);
         }
         if (Input.GetKey("d"))
         {
             pos.x += speed * Time.deltaTime;
+            animator.SetFloat("MoveX", 1);
         }
         if (Input.GetKey("a"))
         {
             pos.x -= speed * Time.deltaTime;
+            animator.SetFloat("MoveX", -1);
         }
 
 
         transform.position = pos;
+        */
     }
 }
