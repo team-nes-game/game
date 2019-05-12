2562412e411364056a5eaf969d906cf8f14c6c6f
Ethan Quick <efquick97@gmail.com>
2019-05-11 23:45:34 -0700

    Added attack animations
    Player can now attack in all directions
    Also updated how player movement and attacking is dealt with. (It now uses an enum and input is gotten in update and dealt with in fixed update).

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,61 +11,59 @@ public class PlayerController : MonoBehaviour
     private bool alive;
     private Animator animator;
     private Vector3 movement;
+    private enum State {walk, attack, dead};
+    private State status;
 
     // Start is called before the first frame update
     void Start()
     {
     	cur_health = MAX_HEALTH;
-    	alive = true;
     	animator = GetComponent<Animator>();
     	self = GetComponent<Rigidbody2D>();
+    	status = State.walk;
+    }
+
+    private void Update(){
+    	//get input here
+    	movement = Vector3.zero;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if(Input.GetButtonDown("Attack") && status != State.attack){
+        	status = State.attack;
+        }
     }
 
     private void FixedUpdate()
     {
     	if(cur_health <= 0){
     		print("Player killed");
-    		alive = false;
+    		status = State.dead;
     	}
+    	//deal with input here
+    	//can probably be a switch
+        if(status == State.attack){
+        	StartCoroutine(AttackRoutine());
+        }
+        else if(status == State.walk){
+        	if(movement != Vector3.zero){
+        		Vector2 pos = transform.position;
+	        	self.MovePosition(transform.position + movement * speed * Time.deltaTime);
+	        	animator.SetFloat("MoveX", movement.x);
+	        	animator.SetFloat("MoveY", movement.y);
+	        	animator.SetBool("Walking", true);
+        	}
+	        else{
+	        	animator.SetBool("Walking", false);
+	        }
+	    }
+    }
 
-        Vector2 pos = transform.position;
-
-        movement = Vector3.zero;
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        if(movement != Vector3.zero){
-        	self.MovePosition(transform.position + movement * speed * Time.deltaTime);
-        	animator.SetFloat("MoveX", movement.x);
-        	animator.SetFloat("MoveY", movement.y);
-        	animator.SetBool("Walking", true);
-        }
-        else{
-        	animator.SetBool("Walking", false);
-        }
-        /*
-        if (Input.GetKey("w"))
-        {
-            pos.y += speed * Time.deltaTime;
-            animator.SetFloat("MoveY", 1);
-        }
-        if (Input.GetKey("s"))
-        {
-            pos.y -= speed * Time.deltaTime;
-            animator.SetFloat("MoveY", -1);
-        }
-        if (Input.GetKey("d"))
-        {
-            pos.x += speed * Time.deltaTime;
-            animator.SetFloat("MoveX", 1);
-        }
-        if (Input.GetKey("a"))
-        {
-            pos.x -= speed * Time.deltaTime;
-            animator.SetFloat("MoveX", -1);
-        }
-
-
-        transform.position = pos;
-        */
+    private IEnumerator AttackRoutine(){
+    	animator.SetBool("Attacking", true);
+    	status = State.attack;
+    	yield return null;
+    	animator.SetBool("Attacking", false);
+    	yield return new WaitForSeconds(.3f);
+    	status = State.walk;
     }
 }
