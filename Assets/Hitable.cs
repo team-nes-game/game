93944fd7f0376097a2b4cba6c9fcdf41dc9c4527
Ethan Quick <efquick97@gmail.com>
2019-05-13 19:40:41 -0700

    Updated player attack and chest
    Chest now opens when hit.

---
+++ Assets/Hitable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitable : MonoBehaviour
+{
+	private Animator animator;
+
+	void Start(){
+		animator = GetComponent<Animator>();
+	}
+
+	public void OnHit(){
+		animator.SetBool("Hit", true);
+	}
+}
